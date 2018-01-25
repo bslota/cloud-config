0ad314ccb530b0951d6cd29f348402ea3a6e1b0d
Bartłomiej Słota <bartlomiej.slota@hl.co.uk>
2018-01-25 13:58:29 +0100

    Discovery first bootstrap

--- gateway/src/test/java/com/bslota/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/bslota/gateway/GatewayApplicationTests.java
@@ -3,10 +3,12 @@ package com.bslota.gateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("integrationTest")
 public class GatewayApplicationTests {
 
 	@Test
--- product-manager/src/test/java/com/bslota/productmanager/ProductManagerApplicationTests.java
+++ product-manager/src/test/java/com/bslota/productmanager/ProductManagerApplicationTests.java
@@ -3,10 +3,12 @@ package com.bslota.productmanager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("integrationTest")
 public class ProductManagerApplicationTests {
 
 	@Test
